50817c4c44c4aec92761af26026565bb286b1674
Usuario <Usuario@DESKTOP-STUV9M6>
2022-07-13 14:59:01 -0300

    Agregar archivos de proyecto.

---
+++ ejerciio1deiniguezoscar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+
+	class ejecicio01deiniguezoscar
+	{
+
+		static void Main(string[] args)
+		{
+			double dolaroficial;
+			double dolarparalelo;
+			double euro;
+			int pesos;
+			Console.Write(" ingrese la cantidad de dinero en pesos:");
+			pesos = int.Parse(Console.ReadLine());
+			dolaroficial = pesos * 230;
+			dolarparalelo = pesos * 280;
+			euro = dolaroficial * dolarparalelo;
+			Console.WriteLine(" en el mercado de dolar oficial con pesos podes comprar esta cantidad de dolares:" + dolaroficial);
+			Console.WriteLine(" con pesos podes comprar esta cantidad de dolares paralelo:" + dolarparalelo);
+			Console.WriteLine("con pesos podes comprar esta cantidad de euros :" + euro);
+		}
+
+	}
+
+}
+
